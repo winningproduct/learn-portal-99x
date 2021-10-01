465219a175ab3c24303d7d9aa743217ea9346b39
Vihanga Bandara <vihanga123@gmail.com>
2021-10-01 10:31:59 +0530

    update connection manager to return connection if already available

--- time-trigger/src/shared/repos/mysql/connection.manager.ts
+++ time-trigger/src/shared/repos/mysql/connection.manager.ts
@@ -1,21 +1,30 @@
-import { createConnection } from 'typeorm';
-import { QuestionDraft } from './entity/question_draft';
+import {createConnection, getConnection, getConnectionManager} from 'typeorm';
+import {QuestionDraft} from './entity/question_draft';
+import {KnowledgeArea} from "./entity/knowledgeArea";
+
 export async function initMysql() {
   try {
-    const con = await createConnection({
-      type: 'mysql',
-      host: '',
-      port: 3306,
-      username: '',
-      password: '',
-      database: '',
-      entities: [
-        QuestionDraft
-      ],
-      synchronize: true,
-      logging: false,
-    });
-    return con;
+    if(getConnectionManager().has("default")){
+      return await getConnection();
+    }
+    else {
+      return await createConnection({
+        name: 'default',
+        type: 'mysql',
+        host: '',
+        port: 3306,
+        username: '',
+        password: '',
+        database: '',
+        entities: [
+          QuestionDraft,
+          KnowledgeArea
+        ],
+        synchronize: true,
+        logging: false,
+      });
+    }
+
   } catch (err) {
     throw err;
   }
